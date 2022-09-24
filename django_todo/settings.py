c3378a82a931bee11250f957853485bb7bf2522f
LarisaLG <lgalisheva@gmail.com>
2022-09-24 13:14:35 +0000

    fix typo

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY','django-insecure-o+(unsd1t_1-dw4syq7##y5p)u58b6a#2(8qg*f=c2syf#s-@+')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-o+(unsd1t_1-dw4syq7##y5p)u58b6a#2(8qg*f=c2syf#s-@+')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
